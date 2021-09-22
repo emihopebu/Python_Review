37349c69b21c69a02808b30c5e72b0ce83bbffdd
courageoushope <emihopebu@gmail.com>
2021-09-22 16:42:50 -0500

    Adding files

---
+++ strings.py
@@ -0,0 +1,12 @@
+#Strings
+print("Hello world!")
+full_name="     Jonny smith"
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(full_name.strip())
+print(full_name)
+full_name=full_name.title().strip()
+print(full_name)
+
---
+++ variables.py
@@ -0,0 +1,13 @@
+#Variables intro
+x=5
+print(5)
+print(x)
+print("x")
+message="Hello world."
+print(message)
+message="Goodbye cuel world..."
+print(message)
+#Bad idea to override built in names
+#print="Happy dance."
+#print(print)
+
