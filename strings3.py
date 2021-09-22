70ebce619f327b32a56c21ce807f040ae1447ef9
courageoushope <emihopebu@gmail.com>
2021-09-22 16:59:35 -0500

    update

---
+++ strings3.py
@@ -0,0 +1,9 @@
+#Strings Part 3
+lyric="Sometimes is seen a strange spot in the sky..."
+lyric=lyric.lower()
+s_count=lyric.count('s')
+print(s_count)
+print("Our lyric has", s_count, "s' in it.")
+print("Our lyric has", s_count, "s' in it.", sep=":)")
+print("Our lyric has " +str(s_count)+ " s' in it.")
+                
