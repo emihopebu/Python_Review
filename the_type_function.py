5b77121fca8768520f94eae73bfbc123818b8bb8
courageoushope <emihopebu@gmail.com>
2021-09-22 20:28:52 -0500

    Adding files

---
+++ the_type_function.py
@@ -0,0 +1,17 @@
+#the type() function
+name="john"
+fav_num=13
+tax_rate=0.4
+print(type(name))
+print(type(fav_num))
+print(type(tax_rate))
+
+tax_rate=str(tax_rate)
+print(type(tax_rate))
+print(name.title()+ "'s favourite number is " + str(fav_num)+".")
+print(type(fav_num))
+print(name.title(), "'s favourite number is", fav_num, ".")
+added_value=fav_num+5
+print(added_value)
+
+
