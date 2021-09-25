d6094ab80fb4a5488dc188201a8614e71f516946
courageoushope <emihopebu@gmail.com>
2021-09-25 16:45:55 -0500

    adding files

---
+++ break_continue_pass.py
@@ -0,0 +1,14 @@
+#Break Continue and Pass
+
+for i in range(1,11):
+    if i==8:
+        break
+    elif i==5:
+        continue
+    elif i==6:
+        pass
+    print(i)
+
+#break statement stops the whole loop
+#continue statement stops the current iteration of the loop
+#pass statement passes over the current statement and continues the loop as normal
