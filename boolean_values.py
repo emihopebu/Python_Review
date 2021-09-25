691117ae204f0ab59a7234aa6357432e0d12096e
courageoushope <emihopebu@gmail.com>
2021-09-25 14:47:36 -0500

    Adding files

---
+++ boolean_values.py
@@ -0,0 +1,35 @@
+#Boolean Values
+
+status=True
+print(status)
+print(type(status))
+
+status=False
+print(status)
+print(type(status))
+
+#=assignment operator
+#==comparison operator
+
+soda='coke'
+print(soda=='coke')
+print(soda=='pepsi')
+print(soda=='Coke')
+print(soda!='root beer')
+
+names=['mike','john','mary']
+mike_status='mike' in names
+bill_status='bill' in names
+print(mike_status)
+print(bill_status)
+
+not_bill_status='bill' not in names
+print(not_bill_status)
+
+
+
+
+
+
+
+
