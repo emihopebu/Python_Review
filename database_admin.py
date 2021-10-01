a6474b586187bb979b434cc85b381b764c3d3f2c
courageoushope <emihopebu@gmail.com>
2021-09-30 22:12:28 -0500

    Adding file

---
+++ database_admin.py
@@ -0,0 +1,18 @@
+#Database Admin Program
+
+print("Welcome to the Database Admin Program.")
+
+#create a dictionary to hold all username:password key-value pairs
+
+log_on_information={
+    "mooman74":"alskes145",
+    "meramo1985":"kehns010101",
+    "nickyD":"worldstar",
+    "george2":"booo3oha",
+    "admin00":"admin1234",}
+
+#get user input
+username=input("Enter your username: ")
+
+#simulate logging on...
+#get user password
