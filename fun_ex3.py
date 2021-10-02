341eb6caccf98b71b44fe5d948a66e4277bc7515
courageoushope <emihopebu@gmail.com>
2021-10-02 14:11:02 -0500

    Adding files

---
+++ fun_ex3.py
@@ -0,0 +1,5 @@
+#function that takes 3 parameters and two optional ones
+
+def opt(a,b,c,d=3,e=4):
+    pass
+    
---
+++ fun_ex4.py
@@ -0,0 +1,12 @@
+#function that takes a string as a parameter,coverts it to a float and returns the result
+
+def convert(a):
+    """
+    string has to be a number "100"
+    
+    """
+    result=float(a)
+    return result
+
+converter=convert("888")
+print(type(converter))
