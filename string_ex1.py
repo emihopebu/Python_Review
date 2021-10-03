47d4124c725aa36adf872b65797975e1a0b3ee19
courageoushope <emihopebu@gmail.com>
2021-10-03 17:20:16 -0500

    Adding files

---
+++ string_ex1.py
@@ -0,0 +1,26 @@
+
+print("cat" in "cat in the hat")
+print("bat" in "cat in the hat")
+print("potter" not in "harry")
+#escaping
+print("She said \"Surely.\"")
+
+#new line
+
+print("line1\nline2\nline3")
+
+fict=["Tolstoy","Camus","Orwell","Huxley","Austin"]
+print(fict[0:3])
+ivan="""In place of death there was light."""
+print(ivan[0:17])
+print(ivan[17:33])
+print(ivan[:17])
+print(ivan[17:])
+print(ivan[:])
+
+equ="All animals are equal."
+equ=equ.replace("a","@")
+print(equ)
+print("animals".index("m"))
+
+
