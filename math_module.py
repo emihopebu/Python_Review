600fb9c5f1eaadfad672f777bdf11bb161035493
courageoushope <emihopebu@gmail.com>
2021-10-03 21:05:47 -0500

    Adding files

---
+++ math_module.py
@@ -0,0 +1,16 @@
+#modules
+
+import math
+import random
+import statistics
+import keyword
+
+print(math.pow(2,3))
+print(random.randint(0,100))
+nums=[1,2,3,4,5,6]
+print(statistics.mean(nums))
+print(statistics.median(nums))
+print(statistics.mode(nums))
+
+print(keyword.iskeyword("for"))
+print(keyword.iskeyword("football"))
