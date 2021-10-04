5c45c98444f187e959d2e561a6ddf85730d2ed2d
courageoushope <emihopebu@gmail.com>
2021-10-03 19:53:39 -0500

    Update

--- for_loop_ex1.py
+++ for_loop_ex1.py
@@ -7,3 +7,18 @@ for character in name:
 shows=["GOT","Narcos","Vice"]
 for show in shows:
     print(show)
+
+coms=("A.Development","Friends","Always Sunny")
+for show in coms:
+    print(show)
+
+people={"Kings of Leon":"Closer",
+        "Zayn":"Common",
+        "Lara Fabian":"Adagio",}
+for character in people:
+    print(character)
+
+
+
+
+
