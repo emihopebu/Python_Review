6e5920aa506a8a16ba87d627a4d696dd164d3de7
courageoushope <emihopebu@gmail.com>
2021-10-05 15:12:03 -0500

    Adding files

---
+++ data types/escape_sequence.py
@@ -0,0 +1,13 @@
+#Escape Sequence
+
+#weather='It's sunny'
+weather="It's sunny"
+#weather="It's "kind of" sunny"
+weather="It's \"kind of\" sunny"
+print(weather)
+weather="\tIt's \"kind of\" sunny"
+print(weather)
+weather="\tIt's \"kind of\" sunny\nhope you have a good day!"
+print(weather)
+
+      
