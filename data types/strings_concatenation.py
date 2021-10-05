afeed20ed0609632b128d4e4af1f32fddd02335f
courageoushope <emihopebu@gmail.com>
2021-10-05 15:02:06 -0500

    Adding files

---
+++ data types/strings_concatenation.py
@@ -0,0 +1,22 @@
+#strings
+
+print(type("hi hello there 24!"))
+
+username="supercode"
+password="supersecret"
+long_string='''
+WOW
+O O
+---
+'''
+print(long_string)
+
+first_name='Andrei'
+last_name='Neagoie'
+full_name=first_name+ ' '+last_name
+print(full_name)
+
+#string concatenation - adding strings together
+print("hello"+" Andrei.")
+
+
