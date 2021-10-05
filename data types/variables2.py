c853aec77f668dee4e5f84d2247027f09e6b54fb
courageoushope <emihopebu@gmail.com>
2021-10-05 14:37:23 -0500

    Adding files

---
+++ data types/variables2.py
@@ -0,0 +1,31 @@
+#variables
+
+user_iq=190 #snake_case
+user_IQ=180 #case sensitive
+
+
+print(user_iq)
+print(user_IQ)
+
+#we cannot create variables with python keywords
+iq=160
+user_age=iq/4
+print(user_age)
+a=user_age #reassigning 
+print(a)
+
+#constants all in capitals should never be changed
+PI=3.14
+PI=0
+
+#__name__ these are meant to be left alone
+
+#naming variables is one of the most important skills
+
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
+
+
+
