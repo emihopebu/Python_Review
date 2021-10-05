b42ddae12cf62638f3ed82c5e6686b82e9074a7f
courageoushope <emihopebu@gmail.com>
2021-10-05 14:49:52 -0500

    Adding files

---
+++ data types/expressions_statements.py
@@ -0,0 +1,7 @@
+#expressions vs statements
+
+iq=100
+         #iq/5 is an expression because it produces the value
+user_age=iq/5
+
+#statement is an entire line of the code that performs some action
