5bad6eeaf296a41b608e63be63801a246a5650ac
courageoushope <emihopebu@gmail.com>
2021-10-04 22:14:11 -0500

    Adding files

---
+++ data types/data_types.py
@@ -0,0 +1,30 @@
+#fundamental data types
+#int, float, bool, str, list, tuple, set, dict
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))#0.5
+
+print(type(0.000001))
+print(type(0))
+print(type(10.567))#we need more memory to store floats
+
+print(type(20+1.1))
+print(type(9.9+1.1))
+print(2**2)
+
+print(2//4)#returns integer
+
+print(5%4)#returns remainder 1
+
+
+
+
+
+#classes custom types
+
+
+#Specialized Data Types (packages and modules)
+
+
+#None means nothing
