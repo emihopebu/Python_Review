f337d03679eaeb26fb5061ac1f77b3e8b779a2ec
courageoushope <emihopebu@gmail.com>
2021-10-06 22:45:32 -0500

    Adding files

---
+++ data types/string_formatting_ex.py
@@ -0,0 +1,15 @@
+#string formatting exercise
+
+print("Hello {}, your balance is {}.".format("Cindy",50))
+
+print("Hello {0}, your balance is {1}.".format("Cindy",50))
+
+print("Hello {name}, your balance is {amount}.".format(name="Cindy",amount=50))
+
+print("Hello {0}, your balance is {amount}.".format("Cindy",amount=50))
+
+name="Cindy"
+amount=50
+
+print(f"Hello {name}, your balance is {amount}.")
+
