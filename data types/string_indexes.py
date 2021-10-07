ddbf94dff42eda79c3e8e7f60a940a8fc72dd0cc
courageoushope <emihopebu@gmail.com>
2021-10-06 22:54:59 -0500

    Adding files

---
+++ data types/string_indexes.py
@@ -0,0 +1,30 @@
+#string indexes
+
+selfish="me me me"
+        #01234567
+#[start:stop]
+print(selfish[0])
+print(selfish[7])
+numbers="01234567"
+print(numbers[0:2])
+print(numbers[0:7])
+print(numbers[0:8])
+#[start:stop:stepover] default is 1
+print(numbers[0:8:2])
+
+print(numbers[1:])#start at 1 and go all the way till the end
+print(numbers[:5])#starts as default 0 and goes till 5
+print(numbers[::1])#default
+
+print(numbers[-1])#starts at the end
+
+print(numbers[-3])
+print(numbers[::-1])#reverse of the string
+print(numbers[::-2])
+
+
+
+
+
+
+
