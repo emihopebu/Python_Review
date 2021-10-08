52cecf454a206d1f2a249988fb1ee959add59064
courageoushope <emihopebu@gmail.com>
2021-10-08 14:26:44 -0500

    Adding files

---
+++ data types/string_indexes_ex.py
@@ -0,0 +1,14 @@
+#string indexes exercise
+
+python="I am Python"
+       #012345678910
+
+print(python[1:4]) # am
+print(python[1:])  # am Python
+print(python[:])   # all
+print(python[1:100])# am Python
+print(python[-1]) #n
+print(python[-4]) #t
+print(python[:-3]) #I am Pyt
+print(python[-3:]) #hon
+print(python[::-1]) #I am Python reverse
