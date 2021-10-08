3e6fd8abc8eda9de8f964eda4f8132a7bf789c4b
courageoushope <emihopebu@gmail.com>
2021-10-08 14:48:58 -0500

    Adding files

---
+++ data types/immutability.py
@@ -0,0 +1,13 @@
+selfish="01234567"
+        #01234567
+#strings in python are immutable
+
+selfish=100
+print(selfish)
+
+#selfish[0]='100'
+#print(selfish)does not work
+selfish='100'
+print(selfish)
+selfish=selfish+"8"
+print(selfish)
