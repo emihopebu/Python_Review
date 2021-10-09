bee1f01e29b22c9ce073095b32ebb644485e9216
courageoushope <emihopebu@gmail.com>
2021-10-09 16:00:41 -0500

    Adding files

---
+++ data types/common_list_patterns.py
@@ -0,0 +1,10 @@
+#common list patterns
+basket=["a","b","c","d","e","d"]
+print(len(basket))
+print(basket[::-1]) #list slicing returns a value
+
+print(list(range(1,101)))
+
+#combining a list into a string
+new_sentence=" ".join(["hi","my","name","is","Jojo"]) #join takes iterable
+print(new_sentence)
