2db31fade6097f60df82096c2c005b236b89af82
courageoushope <emihopebu@gmail.com>
2021-10-09 15:33:15 -0500

    Adding files

---
+++ data types/list_methods2.py
@@ -0,0 +1,11 @@
+#list methods second part
+
+basket=["a","d","b","c","d","e"]
+print(basket.index("d")) #what index is number d
+print(basket.index("c",0, 4)) #where to start and stop
+print("e" in basket)
+print("x" in basket)
+print("i" in "hi my name is Ian")
+print(basket.count("d")) #how many times value occurs in the list
+
+
