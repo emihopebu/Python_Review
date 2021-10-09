00d85a7bece191d2e0adcc1c4f88742accb57951
courageoushope <emihopebu@gmail.com>
2021-10-09 14:36:51 -0500

    Adding files

---
+++ data types/into_lists.py
@@ -0,0 +1,12 @@
+#lists - collection items
+li=[1,2,3,4,5]
+li2=["a","b","c"]
+li3=[1,2,"a",True]
+
+#data structure - container for the data
+
+amazon_cart=["notebook","sunglasses"]
+print(amazon_cart[0])
+print(amazon_cart[1])
+
+
