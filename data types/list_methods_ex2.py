db500e613f28aa8d255465919ad541ecd943c461
courageoushope <emihopebu@gmail.com>
2021-10-09 16:09:58 -0500

    Adding files

---
+++ data types/list_methods_ex2.py
@@ -0,0 +1,6 @@
+#list methods exercise
+friends=["Simon","Patty","Joy","Carrie","Amira","Chu"]
+
+new_friend=["Stanley"]
+friends=friends+new_friend #or friends.extend(new_friend)
+print(sorted(friends))
