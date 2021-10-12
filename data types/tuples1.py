361caee5d522ebbdedde5af91e991b009f0b706e
courageoushope <emihopebu@gmail.com>
2021-10-11 22:40:42 -0500

    Adding files

---
+++ data types/tuples1.py
@@ -0,0 +1,15 @@
+#tuples are immutable, they are faster than lists
+
+my_tuple=(1,2,3,4,5)
+print(my_tuple[1])
+print(5 in my_tuple)
+
+user={
+    (1,2):[1,2,3],
+    "greet":"hello",
+    "age":20,
+    }
+print(user.items()) #returns tuple
+print(user[(1,2)])
+
+
