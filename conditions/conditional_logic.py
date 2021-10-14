837543506a5fb103ef72c42fcbfa6b3629bcc064
courageoushope <emihopebu@gmail.com>
2021-10-13 21:09:28 -0500

    Update

--- conditions/conditional_logic.py
+++ conditions/conditional_logic.py
@@ -1,7 +1,13 @@
 #conditonal logic
-is_old=True
-is_licenced=True
+is_old=False
+is_licenced=False
 
-if is_old:
-    print("you are old enough to drive")
-print("Checkcheck")
+if is_old and is_licenced:
+    print("you are old enough to drive, and you have a licence.")#only run if both are true
+#elif condition:
+#elif condition:
+else:
+    print("you are not of age!")
+
+print("okokok")
+
