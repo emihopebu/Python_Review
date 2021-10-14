7ca8ec4cf9ca3e270eae861594f4708541f6c69b
courageoushope <emihopebu@gmail.com>
2021-10-13 21:22:52 -0500

    Adding files

---
+++ conditions/truthy_falsy.py
@@ -0,0 +1,14 @@
+#truthy and falsy
+
+print(bool("hello"))
+print(bool(5))
+print(bool(""))
+print(bool(0))
+print(bool(None))
+
+password="123"
+username="Johnny"
+
+if password and username:
+    print("You are logged in")
+    
