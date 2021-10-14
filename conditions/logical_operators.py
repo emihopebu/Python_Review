e6ed2aee922442e8fe8a744376b7e156b20a6792
courageoushope <emihopebu@gmail.com>
2021-10-13 21:40:34 -0500

    Adding files

---
+++ conditions/logical_operators.py
@@ -0,0 +1,20 @@
+#logical operators
+print(4>5)
+print(4<5)
+print(4==5)
+print("hello"=="hello")
+print("a">"b")
+print("a">"A")#Here, 'a' ASCII value is 97 and 'A' ASCII value is 65 that's why 'a' is greater than 'A'.
+print(1<2<3<4)#expression
+print(1<2>3<4)#short circuit
+print(1>=0)
+print(1<=0)
+print(0<=0)
+print(0!=0)
+print(0!=1)
+
+print(not(True))
+
+print(not(False))
+print(not(1==1))
+
