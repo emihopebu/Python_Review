e1a5a285bc84e63315ae78b17ba329807ac48b27
courageoushope <emihopebu@gmail.com>
2021-10-15 11:45:08 -0500

    Adding files

---
+++ loops/while_loop2.py
@@ -0,0 +1,15 @@
+#while loop part 2
+my_list=[1,2,3]
+for item in my_list:
+    print(item)
+
+i=0
+while i < len(my_list):
+    print(my_list[i])
+    i+=1
+
+while True: #to make infinite loop
+    response=input("say something: ")
+    if response=="bye":
+        break
+ 
