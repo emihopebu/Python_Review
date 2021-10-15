02f4498893be0aec014d6787225a2d8ab90ff677
courageoushope <emihopebu@gmail.com>
2021-10-15 12:50:46 -0500

    Adding files

---
+++ loops/first_gui.py
@@ -0,0 +1,18 @@
+#first gui exercise
+
+picture=[
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0],
+    ]
+for line in picture:
+    for num in line:
+        if num==0:
+            print(" ", end="")
+        else:
+            print("*", end="")
+    print("\n")
+    
