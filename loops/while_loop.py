1abb38d506d4b4811f40b34a1f29b3242de36ba5
courageoushope <emihopebu@gmail.com>
2021-10-15 11:38:34 -0500

    Adding files

---
+++ loops/while_loop.py
@@ -0,0 +1,16 @@
+#while loop
+
+i=0
+while i <50:
+    print(i)
+    i+=1 #without this there would be an infinite loop
+else:
+    print("Done with all the work.") #if there is no break above it it will run
+i=0
+while i <50:
+    print(i)
+    break
+
+
+
+    
