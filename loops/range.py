bb5ead79a0d2b5a6242f3a8900a2529589128738
courageoushope <emihopebu@gmail.com>
2021-10-15 11:18:57 -0500

    Adding files

---
+++ loops/range.py
@@ -0,0 +1,20 @@
+#range
+print(range(100)) #range of a 0 to a 100 it is a range object
+print(range(0,100))
+
+for i in range(0,10): #we can iterate over range too
+    print("email email list")
+
+for _ in range(0,11,2):
+    print(_)
+
+for _ in range(11,0,-2):
+    print(_)
+
+for _ in range(10,0,-1):
+    print(_)
+
+print(list(range(10)))# a quick way to create a list
+
+
+
