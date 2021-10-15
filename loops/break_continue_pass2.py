38a2f61e3c261ea2a0f737a4e949c4862b2dbbcb
courageoushope <emihopebu@gmail.com>
2021-10-15 11:51:44 -0500

    Adding files

---
+++ loops/break_continue_pass2.py
@@ -0,0 +1,20 @@
+#break, continue, pass
+#break works in the for loop too
+
+my_list=[1,2,3,4]
+for item in my_list:
+    continue
+    print(item)
+
+i=0
+while i<len(my_list):
+    i+=1
+    continue
+    print(my_list[i])
+for item in my_list:
+    pass #still thinking about it, placeholder
+
+
+
+
+
