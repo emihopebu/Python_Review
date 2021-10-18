56a898909ce72c59c94726c1f63d9fcabf067595
courageoushope <emihopebu@gmail.com>
2021-10-17 22:00:14 -0500

    Adding files

---
+++ functions/arguments_parameters.py
@@ -0,0 +1,12 @@
+#parameters
+
+def say_hello(name, emoji):
+    print(f"Hello {name} {emoji}.")
+
+#arguments
+say_hello("Emi",":)")#invoke the function
+say_hello("Dan",":)")
+say_hello("Emily",":)")
+
+    
+
