265402fb6d9bb5c8ab396d98be6cac03ca7ca64c
courageoushope <emihopebu@gmail.com>
2021-10-22 22:36:46 -0500

    Adding files

---
+++ functions/docstrings3.py
@@ -0,0 +1,15 @@
+#Docstrings part three
+
+def absolute_value(num):
+    """This function returns the absolute
+    value of the entered number"""
+
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+
+print(absolute_value(22))
+
+print(absolute_value(-11))
