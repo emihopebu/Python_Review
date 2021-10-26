38f6b20081a3bd3a5544dbad26d11096666729e0
courageoushope <emihopebu@gmail.com>
2021-10-26 13:57:10 -0500

    Update

---
+++ Python OOP/intro_oop.py
@@ -0,0 +1,18 @@
+#OOP
+
+class BigObject: #Class
+    #code
+    pass
+obj1=BigObject() #instanciate
+obj2=BigObject() #instanciate
+obj3=BigObject() #instanciate
+print(type(obj1))
+
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type("hi"))
+print(type([]))
+print(type(()))
+print(type({}))
