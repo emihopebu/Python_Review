b06c7393da0f135bb9220d3e6998db13c3a6e53a
courageoushope <emihopebu@gmail.com>
2021-10-28 16:23:31 -0500

    Adding files

---
+++ Python Basics/tuples3.py
@@ -0,0 +1,25 @@
+#tuples
+
+t=(1,2,3)
+mylist=[1,2,3]
+
+print(len(t))
+print(len(mylist))
+
+t2=("hello",22)
+
+print(t2[0])
+print(t2[-1])
+
+t3=("a","a","b")
+
+print(t3.count("a"))
+
+print(t3.index("b"))
+print(t3.index("a"))
+
+mylist[0]="New"
+print(mylist)
+
+#t3[0]="NEW" immutable
+
