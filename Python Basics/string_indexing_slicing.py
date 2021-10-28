43607a5eb739a726a0cd3700230ff4649a554125
courageoushope <emihopebu@gmail.com>
2021-10-28 09:39:13 -0500

    Adding files

---
+++ Python Basics/string_indexing_slicing.py
@@ -0,0 +1,22 @@
+#string indexing
+
+mystring="Hello World"
+print(mystring[0])
+print(mystring[8])
+print(mystring[-2])
+
+#string slicing
+mystring2="abcdefghijk"
+print(mystring2[2:])
+print(mystring2[:3])
+print(mystring2[3:6])
+print(mystring2[1:3])
+
+print(mystring2[::2])
+print(mystring2[2:7:2])
+
+print(mystring2[::-1])
+
+
+
+
