f3a2928c84e9beae48c602542d54361c4267d89e
emihopebu <emihopebu@gmail.com>
2021-11-04 15:14:14 -0500

    Adding files

---
+++ chaining_operators.py
@@ -0,0 +1,17 @@
+#chaining comparison operators with logical operators
+
+print(1<2<3)
+print(1<2>3)
+print(1<2 and 2>3)
+
+print(("h"=="h") and (2==2))
+
+print(1==1 or 2==2)
+print(100==1 or 2==2)
+
+print(100==1 or 2==200)
+
+print(not(1==1))
+
+print(not (400> 500))
+
