7080ed2c9ebb85511416ef7bd729558d7f2f2a2d
emihopebu <emihopebu@gmail.com>
2021-11-04 16:31:57 -0500

    Adding files

---
+++ Python Basics/control_flow.py
@@ -0,0 +1,38 @@
+#control flow
+
+if True:
+    print("It is true.")
+if 3>2:
+    print("True.")
+
+hungry="True"
+
+if hungry:
+    print("Feed me.")
+
+thirsty="False"
+
+if thirsty:
+    print("Give me water.")
+else:
+    print("I am not thirsty.")
+
+loc="Bank"
+
+if loc=="Auto Shop":
+    print("Cars are cool.")
+elif loc=="Bank":
+    print("Money is cool.")
+elif loc=="Store":
+    print("Welcome to the store.")
+else:
+    print("I don't know much.")
+
+name="Sammy"
+
+if name=="Frankie":
+    print("Hello Frankie.")
+elif name=="Sammy":
+    print("Hello Sammy.")
+else:
+    print("What is your name?")
